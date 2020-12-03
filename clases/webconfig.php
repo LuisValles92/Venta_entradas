19deebc2f7775d25fe4e99c656e410bacd31a6b4
LuisValles92 <luisvalles92@hotmail.com>
2020-12-03 20:41:46 +0100

    Commit webconfig.php

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,8 +1,6 @@
 <?php
 //define('DNS','mysql:dbname=conciertos;host=node29125-env-9549929.es-1.axarnet.cloud');
-define('DNS','mysql:dbname=conciertos;host=localhost');
+define('DNS','mysql:dbname=conciertos;host=https://node29131-env-9961833.es-1.axarnet.cloud');
 define('USERNAME','root');  
-define('PASSWORD','');
+define('PASSWORD','NLVvie86415');
 ?>
-
-		
