9d70a1987d1b590c482069eaaccbe77ea713c935
LuisValles92 <luisvalles92@hotmail.com>
2020-12-03 20:47:47 +0100

    Commit webconfig.php sin https

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DNS','mysql:dbname=conciertos;host=https://node29131-env-9961833.es-1.axarnet.cloud');
+define('DNS','mysql:dbname=conciertos;host=node29131-env-9961833.es-1.axarnet.cloud');
 define('USERNAME','root');  
 define('PASSWORD','NLVvie86415');
 ?>
