b5e0b1eb56c363e9777d8a59c6cd9794d191d220
Luis VP <luisvalles92@hotmail.com>
2020-12-05 18:26:18 +0100

    css important

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
@@ -9,7 +10,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
     <script>
         (function($) {
             $(function() {
