e58565863cc1ab0ddae9e4541cf1b67ff6596db0
LuisValles92 <58343146+LuisValles92@users.noreply.github.com>
2020-12-22 13:05:37 +0100

    Update webconfig.php

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,7 +1,9 @@
 <?php
-// define('DNS', 'mysql:dbname=conciertos;host=localhost');
-// define('USERNAME', 'root');
-// define('PASSWORD', '');
+define('DNS', 'mysql:dbname=conciertos;host=localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+/*
 define('DNS', 'mysql:dbname=epiz_27503645_conciertos;host=sql200.epizy.com');
 define('USERNAME', 'epiz_27503645');
 define('PASSWORD', 'Tmi3dhOnNmkC');
+*/
